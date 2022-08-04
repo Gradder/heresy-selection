bff94b366ecfc834ef9cb215d87bfc2503fd411a
Gradder <rzhitkov@gmail.com>
2022-08-04 16:52:29 +0200

    Add progress bar

--- heresy-selection-ui/src/components/ProgressBar.js
+++ heresy-selection-ui/src/components/ProgressBar.js
@@ -1,11 +1,17 @@
-import logo from '../assets/images/logo_hh.png';
+import istvaan from '../assets/images/Istvaan_III_planet2.png';
+import terra from '../assets/images/terra.png';
+import ship from '../assets/images/ship4.png';
 import './ProgressBar.css';
 
 function ProgressBar() {
   return (
-    <div className="ProgressBar">
-    progress
-    </div>
+  <div className="ProgressDivBig">
+      <div className="ProgressDiv">
+          <div className="Planet" style={{ backgroundImage: `url(${istvaan})` }}>  </div>
+            <div className="ProgressBar"><div className="Ship" style={{ backgroundImage: `url(${ship})` }}>  </div></div>
+          <div className="Planet Terra" style={{ backgroundImage: `url(${terra})` }}>  </div>
+      </div>
+  </div>
   );
 }
 
